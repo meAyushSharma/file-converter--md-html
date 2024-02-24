54cdf00c7af8b35f311d2b7a9aaec68c5fade876
meAyushSharma <iamayushsharma60@gmail.com>
2024-02-24 17:10:53 +0530

    Added basic functionality for both conversions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import showdown from "showdown";
 import express from "express";
 import path from "path";
+import {JSDOM} from "jsdom";
 
 const app = express();
 const port = 3000;
@@ -16,12 +17,40 @@ app.get("/", (req, res) => {
 
 app.post("/submit-md", (req, res) => {
   const userInput = req.body.userInput;
-  var converter = new showdown.Converter(),
+  var converter = new showdown.Converter({
+      tables: true,
+      simpleLineBreaks: true,
+    }),
     text = userInput,
     htmlUn = converter.makeHtml(text);
   console.log(htmlUn);
+
   res.send(`<h4> Here is you converted string: </h4> <br/>
   <h5> ${htmlUn} </h5>`);
+});
+
+app.post("/submit-html", (req, res) => {
+  const userInput = req.body.userInput;
+//   const dom = new JSDOM(userInput);
+//   var converter = new showdown.Converter({
+//     tables: true,
+//     simpleLineBreaks: true,
+//   }),
+//   text = dom,
+//   mdUn = converter.makeMarkdown(text);
+// console.log(mdUn);
+const convertor = new showdown.Converter();
+    const dom = new JSDOM(userInput);
+    console.log("Parsing HTML");
+    const parsedHTML = dom.window.document;
+    const markdown = convertor.makeMarkdown(userInput, parsedHTML);
+    console.log(markdown);
+
+  res.send(`
+  <h4 style="font-family: Trebunchet MS"> Here is you converted string: </h4> 
+  <br/>
+  <h5> ${markdown} </h5>
+  `);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
